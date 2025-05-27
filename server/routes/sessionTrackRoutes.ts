8210f7077258bf05b057ce01b612482dc7f8b5fc
Madhur Bharti <madhur@Madhurs-MacBook-Pro.local>
2025-05-28 02:44:54 +0530

    validation logic

--- server/routes/sessionTrackRoutes.ts
+++ server/routes/sessionTrackRoutes.ts
@@ -1,8 +1,10 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import userTracking from "../controller/userTracking";
 import { login, logout, register } from "../controller/authController";
 import { getAnalyticsReport } from "../controller/adminController";
 import isAdmin from "../middlewares/admin"
+import { validateUserTracking,validateLogin,validateSignup } from "../middlewares/validation";
+import { validationResult } from "express-validator";
 const router = Router();
 router.get("/session", (req: Request, res: Response) => {
     if (!req.session.userData) {
@@ -12,10 +14,39 @@ router.get("/session", (req: Request, res: Response) => {
 
      res.json(req.session.userData);
 });
-
-router.post('/usertrack',userTracking)
-router.post('/register',register);
-router.post('/login',login)
+router.post(
+  '/usertrack',
+  validateUserTracking,
+  (req: Request, res: Response, next: NextFunction): void => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+    next();
+  },
+  userTracking
+);
+router.post('/register',  validateSignup,
+  (req: Request, res: Response, next: NextFunction): void => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+    next();
+  },
+register);
+router.post('/login',  validateLogin,
+  (req: Request, res: Response, next: NextFunction): void => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+    next();
+  },
+login)
 router.post('/logout',logout);
 router.get('/admin/analytics',isAdmin,getAnalyticsReport)
 export default router;
